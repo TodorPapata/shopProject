c490f1a886eb3291af454e84a7538f7854285f43
Todor <todorpapata@abv.bg>
2017-11-25 17:16:12 +0200

    add frontend

---
+++ src/main/frontend/src/app/products/products.module.ts
@@ -0,0 +1,35 @@
+import { HttpClientModule } from '@angular/common/http';
+import { UploadFileService } from './upload.service';
+import { CatalogService } from './catalog.service';
+import { HttpModule } from '@angular/http';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProductListComponent } from './product-list/product-list.component';
+import { DetailsUploadComponent } from './details-upload/details-upload.component';
+import { FormUploadComponent } from './upload-form/form-upload.component';
+import { ListUploadComponent } from './uploads-list/list-upload.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    HttpModule,
+    HttpClientModule
+  ],
+  declarations: [
+    ProductListComponent,
+    DetailsUploadComponent,
+    FormUploadComponent,
+    ListUploadComponent
+  ],
+  providers: [
+    CatalogService,
+    UploadFileService
+  ],
+  exports: [
+    ProductListComponent,
+    DetailsUploadComponent,
+    FormUploadComponent,
+    ListUploadComponent
+  ]
+})
+export class ProductsModule { }
